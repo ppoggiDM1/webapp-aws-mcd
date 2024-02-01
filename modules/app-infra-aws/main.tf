7b4392dfe2b9dac6c34bc6c2a512b8f7bff26072
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-01 12:48:30 +0100

    c18

--- modules/app-infra-aws/main.tf
+++ modules/app-infra-aws/main.tf
@@ -150,28 +150,7 @@ resource "aws_instance" "ec2_frontend" {
    tags = {
       Name = "frontend-${count.index}"
    }
-   user_data               = <<EOF
-#!/bin/bash
-EC2-instance on AWS
-# Update Repos
-#!/bin/bash
-yum update -y
-
-yum install -y git
-
-# Install Docker
-yum install -y docker
-id ec2-user
-# Reload a Linux user's group assignments to docker w/o logout
-newgrp docker
-systemctl status docker.service
-
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
-
-       EOF
-
+   user_data      = file("${path.module}/scripts/install-dependencies.sh")
 }
 
 # 2nd ec2 instance on public subnet 1
--- modules/app-infra-aws/scripts/install-dependencies.sh
+++ modules/app-infra-aws/scripts/install-dependencies.sh
@@ -4,10 +4,16 @@ EC2-instance on AWS
 #!/bin/bash
 yum update -y
 
+yum install -y git
+
 # Install Docker
 yum install -y docker
 id ec2-user
 # Reload a Linux user's group assignments to docker w/o logout
 newgrp docker
 systemctl status docker.service
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
 
